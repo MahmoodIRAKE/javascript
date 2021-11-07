406225c56dec3880551482970990669215ff54c2
MahmoodIRAKE <mhmod.eraqi@gmail.com>
2021-11-07 16:37:08 +0200

    javascript

---
+++ 1.1-primitave-data.js
@@ -0,0 +1,13 @@
+let str='mahmood';
+console.log(str);
+let b=1;
+console.log(b);
+let bool=true;
+console.log(bool);
+let obj=new Array(5);
+console.log(obj;
+let no=null;
+console.log(no);
+let un=undefined;
+console.log(un);
+
